4284ad5dc40fb730b8a6137625320150b7c1105d
lachlan <lachlang93@gmail.com>
2022-03-04 11:31:02 +1100

    added cron job fucntion

--- B/asx-db-sl/dev/dev.py
+++ B/asx-db-sl/dev/dev.py
@@ -6,9 +6,21 @@ from jinja2 import Environment, FileSystemLoader
 import yahoo_fin.stock_info as si
 import pandas as pd
 import csv 
+import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
+def autoUpdate(event, context):
+    current_time = datetime.datetime.now().time()
+    name = context.function_name
+    logger.info("Your cron function " + name + " ran at " + str(current_time)) 
 
+#################
+# H E L P E R S #
+#################
 
 def clean(data):
     data.columns = data.columns.astype(str)
@@ -17,4 +29,4 @@ def clean(data):
     data = data.astype('Int64')
     data = pd.DataFrame.to_dict(data)
     return(data)
-    
+
