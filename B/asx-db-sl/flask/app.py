0b8e58be24703c45bf293e4f69a67fa59b9eed8b
lachlan <lachlang93@gmail.com>
2022-03-09 00:25:48 +1100

    added routes & templates for cash flow, income statement, balance sheet

--- B/asx-db-sl/flask/app.py
+++ B/asx-db-sl/flask/app.py
@@ -98,15 +98,74 @@ def get_next_tickers():
     
 
 @app.route('/ticker/<string:ticker>')
-def get_ticker(ticker):
+@app.route('/ticker/<string:ticker>/info')
+def view_info(ticker):
    ticker = ticker.upper()
-   response = table.query(
-       KeyConditionExpression=Key('ticker').eq(ticker)
+   response = table.get_item(
+       Key={
+           'ASX code': ticker
+           }
    )
+  
+   print(response)
+   headers = {
+        "sector": "Sector:", 
+        "industry": "Industry:",
+        "website": "Website:",
+        "address1": "Address:",  
+        "city": "City:",
+        "state": "State:",
+        "phone": "Phone Number:",
+        "zip": "Postcode:",
+        "country": "Country:",
+    } 
    data = {
        'page_title': 'Ticker Details',
+       'ticker': response['Item'],
+       
    } 
-   return(response)
+
+   return(render_template('info.html', page_data=data, headers=headers))
+
+@app.route('/ticker/<string:ticker>/cash_flow')
+def view_cash_flow(ticker):
+    ticker = ticker.upper()
+    response = table.get_item(
+        Key={
+            'ASX code': ticker
+        }
+    )
+    data = {
+        'page_title': 'Cash Flow',
+        'ticker': response['Item'],
+    }
+    return render_template('cash_flow.html', page_data=data)
+@app.route('/ticker/<string:ticker>/balance_sheet')
+def view_balance_sheet(ticker):
+    ticker = ticker.upper()
+    response = table.get_item(
+        Key={
+            'ASX code': ticker
+        }
+    )
+    data = {
+        'page_title': 'Balance Sheet',
+        'ticker': response['Item'],
+    }
+    return render_template('balance_sheet.html', page_data=data)
+@app.route('/ticker/<string:ticker>/income_statement')
+def view_income_statement(ticker):
+    ticker = ticker.upper()
+    response = table.get_item(
+        Key={
+            'ASX code': ticker
+        }
+    )
+    data = {
+        'page_title': 'Income Statement',
+        'ticker': response['Item'],
+    }
+    return render_template('income_statement.html', page_data=data)
 
 @app.errorhandler(404)
 def resource_not_found(e):
