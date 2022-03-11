3aaad4ba2b80af4131f781876c0d4a96000a18f2
lachlan <lachlang93@gmail.com>
2022-03-12 02:16:42 +1100

    imported display of tables

--- B/asx-db-sl/flask/controllers/ticker_controller.py
+++ B/asx-db-sl/flask/controllers/ticker_controller.py
@@ -1,54 +1,53 @@
-from flask import Blueprint, render_template, request
-from helpers import get_item
+from flask import Blueprint, render_template, request, flash, redirect, url_for 
+from helpers import get_item, hover, get_table
 import pandas as pd
 
 ticker = Blueprint('ticker', __name__)
 
 
-@ticker.route('/ticker/', methods=['post'])
+@ticker.route('/ticker/', methods=['POST'])
 @ticker.route('/ticker/<string:ticker>')
 @ticker.route('/ticker/<string:ticker>/info')
-def view_info():
-
-   if request.method == 'POST':
-       ticker = request.form['ticker']
-       print(ticker)
+def view_info(ticker=None):
+    if request.method == 'POST':
+        ticker = request.form['ticker']
        
-   ticker = ticker.upper()
-   response = get_item(ticker)
+    ticker = ticker.upper()
+    try: 
+        response = get_item(ticker)
   
-   headers = {
-        "sector": "Sector:", 
-        "industry": "Industry:",
-        "website": "Website:",
-        "address1": "Address:",  
-        "city": "City:",
-        "state": "State:",
-        "phone": "Phone Number:",
-        "zip": "Postcode:",
-        "country": "Country:",
-    } 
-   data = {
-       'page_title': 'Ticker Details',
-       'ticker': response['Item'],
-       
-   } 
-
-   return(render_template('info.html', page_data=data, headers=headers))
+        headers = {
+                "sector": "Sector:", 
+                "industry": "Industry:",
+                "website": "Website:",
+                "address1": "Address:",  
+                "city": "City:",
+                "state": "State:",
+                "phone": "Phone Number:",
+                "zip": "Postcode:",
+                "country": "Country:",
+            } 
+        data = {
+            'page_title': 'Ticker Details',
+            'ticker': response['Item'],
+        } 
+        return(render_template('info.html', page_data=data, headers=headers))
+    except:
+       flash('Sorry, we could not find the ticker you requested. Try Again', 'error')
+       return redirect('/search')
 
 @ticker.route('/ticker/<string:ticker>/cash_flow')
 def view_cash_flow(ticker):
     ticker = ticker.upper()
     response = get_item(ticker)
-    cashflow = pd.DataFrame(response['Item']['Cash Flow'])
+    df_html = get_table(response)
 
-    data = {
+    data ={
         'page_title': 'Cash Flow',
-        'ticker': response['Item'],
+        'ticker': response['Item']
     }
-    print(cashflow.style)
 
-    return render_template('cash_flow.html', page_data=data, tables=[cashflow.to_html(classes='data', header='true')])
+    return render_template('cash_flow.html', page_data=data, table=df_html)
 
 @ticker.route('/ticker/<string:ticker>/balance_sheet')
 def view_balance_sheet(ticker):
