0133d0ae521427013d2d53304f362d3915bbf7e0
lachlan <lachlang93@gmail.com>
2022-03-12 02:14:22 +1100

    added development dependencies

--- B/asx-db-sl/flask/app.py
+++ B/asx-db-sl/flask/app.py
@@ -4,6 +4,19 @@ from flask import Flask, jsonify, make_response, render_template, request, sessi
 import datetime
 import csv
 
+development = True
+if development == True:
+    import os
+    import boto3
+    import yahoo_fin.stock_info as si
+    import pandas as pd
+    import csv 
+    import datetime
+    import logging
+    from boto3.dynamodb.conditions import Key
+    from threading import Thread
+    import concurrent.futures
+
 app = Flask(__name__)
 
 SECRET_KEY = os.urandom(12)
@@ -35,109 +48,110 @@ def handle_500(e):
 @app.errorhandler(403)
 def handle_403(e):
     return "Error 403: Forbidden", 403
-'''
-#######################################################
-# These Functions Are Used for Local Development only #
-#######################################################
-@app.route('/update/<string:ticker>', methods=['POST'])
-def add(ticker):
+
+if development: 
+    #######################################################
+    # These Functions Are Used for Local Development only #
+    #######################################################
+    @app.route('/update/<string:ticker>', methods=['POST'])
+    def add(ticker):
 
 
-    key = {'ASX code': ticker}
-    ticker = ticker + '.AX'
-    info = pd.DataFrame.to_dict(si.get_company_info(ticker))
-    info = info['Value']
-
-    cash_flow = clean(si.get_cash_flow(ticker))
-    income_statement = clean(si.get_income_statement(ticker))
-    balance_sheet = clean(si.get_balance_sheet(ticker))
-    ticker = ticker[:-3]
-
-    response = table.update_item(
-        Key={
-            'ASX code': ticker
-        },
-        UpdateExpression="set #i = :i, #c = :c, #is = :is, #bs = :bs, #lu = :lu",
-        ExpressionAttributeNames={
-            '#i': 'Info',
-            '#c': 'Cash Flow',
-            '#is': 'Income Statement',
-            '#bs': 'Balance Sheet',
-            '#lu': 'LastUpdated'
-        },
-        ExpressionAttributeValues={
-            ':i': info,
-            ':c': cash_flow,
-            ':is': income_statement,
-            ':bs': balance_sheet,
-            ':lu': get_time()
-        }
-    )
-    return("OK")
-
-@app.route('/test', methods=['POST'])
-def test():
-    # Fetch the oldest entry from the database and return the 'ASX code'.
+        key = {'ASX code': ticker}
+        ticker = ticker + '.AX'
+        info = pd.DataFrame.to_dict(si.get_company_info(ticker))
+        info = info['Value']
 
 
-    ticker = '88E.AX'
-    info = pd.DataFrame.to_dict(si.get_company_info(ticker))
-    info = info['Value']
+        cash_flow = clean(si.get_cash_flow(ticker))
+        income_statement = clean(si.get_income_statement(ticker))
+        balance_sheet = clean(si.get_balance_sheet(ticker))
+        ticker = ticker[:-3]
 
-    return info
+        response = table.update_item(
+            Key={
+                'ASX code': ticker
+            },
+            UpdateExpression="set #i = :i, #c = :c, #is = :is, #bs = :bs, #lu = :lu",
+            ExpressionAttributeNames={
+                '#i': 'Info',
+                '#c': 'Cash Flow',
+                '#is': 'Income Statement',
+                '#bs': 'Balance Sheet',
+                '#lu': 'LastUpdated'
+            },
+            ExpressionAttributeValues={
+                ':i': info,
+                ':c': cash_flow,
+                ':is': income_statement,
+                ':bs': balance_sheet,
+                ':lu': datetime.datetime.utcnow().isoformat()
+            }
+        )
+        return("OK")
 
-
-
-@app.route('/init', methods=['POST'])
-# initialize the database with basic data from all companies listed on the asx
-def init():
-
-    with open('./ASX_Listed_Companies_24-02-2022_09-03-57_AEDT.csv', newline='') as csvfile:
-        tickers_list = csv.reader(csvfile, delimiter=',')
-
-        header = next(tickers_list, None)
-        for ticker in tickers_list:
-            try:
-                response = table.put_item(
-                    Item={
-                    header[0]: ticker[0] or "N/A",
-                    header[1]: ticker[1] or "N/A",
-                    header[2]: ticker[2] or "N/A",
-                    header[3]: ticker[3] or "N/A",
-                    header[4]: try_int(ticker[4]),
-                    'LastUpdated': datetime.datetime.utcnow().isoformat(),
-                    'GSI1PK': 'TICKERS'
-                    }
-                )
-                print(f"added {ticker[0]} to the database")
-            except Exception as e:
-                print({
-                    header[0]: ticker[0],
-                    header[1]: ticker[1],
-                    header[2]: ticker[2], 
-                    header[3]: ticker[3],
-                    header[4]: try_int(ticker[4]),
-                    'LastUpdated': datetime.datetime.utcnow().isoformat(),
-                    'GSI1PK': 'TICKERS'
-                    })
-                print(e)
-                return("Error")
-
-    return(jsonify({"status": "init success"}))
-
-def try_int(data):
-    try:
-        data = int(data)
-    except:
-        data = 0
-    return(data)
+    @app.route('/test', methods=['POST'])
+    def test():
+        # Fetch the oldest entry from the database and return the 'ASX code'.
 
 
-def clean(data):
-    data.columns = data.columns.astype(str)
-    data = data.fillna(0)
-    data = data.astype('float')
-    data = data.astype('Int64')
-    data = pd.DataFrame.to_dict(data)
-    return(data)
-'''
+        ticker = '88E.AX'
+        info = pd.DataFrame.to_dict(si.get_company_info(ticker))
+        info = info['Value']
+
+        return info
+
+
+
+    @app.route('/init', methods=['POST'])
+    # initialize the database with basic data from all companies listed on the asx
+    def init():
+
+        with open('./ASX_Listed_Companies_24-02-2022_09-03-57_AEDT.csv', newline='') as csvfile:
+            tickers_list = csv.reader(csvfile, delimiter=',')
+
+            header = next(tickers_list, None)
+            for ticker in tickers_list:
+                try:
+                    response = table.put_item(
+                        Item={
+                        header[0]: ticker[0] or "N/A",
+                        header[1]: ticker[1] or "N/A",
+                        header[2]: ticker[2] or "N/A",
+                        header[3]: ticker[3] or "N/A",
+                        header[4]: try_int(ticker[4]),
+                        'LastUpdated': datetime.datetime.utcnow().isoformat(),
+                        'GSI1PK': 'TICKERS'
+                        }
+                    )
+                    print(f"added {ticker[0]} to the database")
+                except Exception as e:
+                    print({
+                        header[0]: ticker[0],
+                        header[1]: ticker[1],
+                        header[2]: ticker[2], 
+                        header[3]: ticker[3],
+                        header[4]: try_int(ticker[4]),
+                        'LastUpdated': datetime.datetime.utcnow().isoformat(),
+                        'GSI1PK': 'TICKERS'
+                        })
+                    print(e)
+                    return("Error")
+
+        return(jsonify({"status": "init success"}))
+
+    def try_int(data):
+        try:
+            data = int(data)
+        except:
+            data = 0
+        return(data)
+
+
+    def clean(data):
+        data.columns = data.columns.astype(str)
+        data = data.fillna(0)
+        data = data.astype('float')
+        data = data.astype('Int64')
+        data = pd.DataFrame.to_dict(data)
+        return(data)
